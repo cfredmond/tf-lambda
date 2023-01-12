a93caf25afab57ca6b030300c0108bdabacfdcd4
Charles Redmond <charles.redmond@gmail.com>
2023-01-12 14:30:31 -0500

    updates

--- example/example.js
+++ example/example.js
@@ -1,61 +1,8 @@
-const randomBytes = require('crypto').randomBytes;
-
-const AWS = require('aws-sdk');
-
-const ddb = new AWS.DynamoDB.DocumentClient();
-
-exports.handler = (event, context, callback) => {
-    const requestBody = JSON.parse(event.body);
-
-    let reading = {
-        ReadingId: toUrlString(randomBytes(16)),
-        date: new Date().toISOString(),
-        sensorId: requestBody.SensorId,
-        temperature: requestBody.temperature,
-        pressure: requestBody.pressure,
-        humidity: requestBody.humidity,
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
     };
-
-    saveReading(reading).then(() => {
-        callback(null, {
-            statusCode: 201,
-            body: JSON.stringify({
-                //
-            }),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-        });
-    }).catch((err) => {
-        console.error(err);
-
-        errorResponse(err.message, context.awsRequestId, callback)
-    });
+    return response;
 };
-
-function saveReading(reading) {
-    return ddb.put({
-        TableName: 'Bme280v2',
-        Item: reading,
-    }).promise();
-}
-
-function errorResponse(errorMessage, awsRequestId, callback) {
-  callback(null, {
-    statusCode: 500,
-    body: JSON.stringify({
-      Error: errorMessage,
-      Reference: awsRequestId,
-    }),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
-}
-
-function toUrlString(buffer) {
-    return buffer.toString('base64')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
-}
--- lambda.tf
+++ lambda.tf
@@ -42,54 +42,30 @@ resource "aws_lambda_function" "example" {
   role = aws_iam_role.lambda_exec.arn
 }
 
-# resource "aws_lambda_permission" "api_gw" {
-#   statement_id  = "AllowExecutionFromAPIGateway"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.bme280_save_reading.function_name
-#   principal     = "apigateway.amazonaws.com"
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
 
-#   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role" "lambda_exec" {
-#   name = "serverless_lambda"
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Sid    = ""
-#       Principal = {
-#         Service = "lambda.amazonaws.com"
-#       }
-#       }
-#     ]
-#   })
-# }
+resource "aws_cloudwatch_log_group" "example" {
+  name = "/aws/lambda/${aws_lambda_function.example.function_name}"
 
-# resource "aws_iam_role_policy" "dynamodb_policy" {
-#   name = "dynamodb_policy"
-#   role = aws_iam_role.lambda_exec.id
-  
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Sid = "VisualEditor0"
-#       Effect = "Allow"
-#       Action = "dynamodb:PutItem"
-#       Resource = "arn:aws:dynamodb:us-east-1:525067006432:table/Bme280v2"
-#     }]
-#   })  
-# }
-
-# resource "aws_iam_role_policy_attachment" "lambda_policy" {
-#   role       = aws_iam_role.lambda_exec.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
-
-# resource "aws_cloudwatch_log_group" "bme280_save_reading" {
-#   name = "/aws/lambda/${aws_lambda_function.bme280_save_reading.function_name}"
-
-#   retention_in_days = 30
-# }
+  retention_in_days = 30
+}
