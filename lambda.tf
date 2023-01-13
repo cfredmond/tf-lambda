f228a32de83d262d6c9f8588b4da37fd1482de2d
Charles Redmond <charles.redmond@gmail.com>
2023-01-12 22:30:42 -0500

    Converting to python (#2)
    * changed example to python
    
    * updated lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "example" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key    = aws_s3_object.example.key
 
-  runtime = "nodejs16.x"
+  runtime = "python3.9"
   handler = "example.handler"
 
   source_code_hash = data.archive_file.example.output_base64sha256
