1b6b45093ec39bfb54cdd7cc9eebec29c411f93d
Charles Redmond <charles.redmond@gmail.com>
2023-01-13 00:41:01 -0500

    added build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+# copy lib code
+cp -R example/ example.copy/
+
+# copy lib code
+cp -R venv/lib/python3.10/site-packages/* example/
+
+# terraform init
+terraform init
+
+# terraform apply
+terraform init
+
+# delete lib code
+rm -rf example
+mv example.copy/ example/
